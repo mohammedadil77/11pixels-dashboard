591e23506ef1842ec054beebd1f7d725e3e3dfc1
Mohammed Adil <mohammed.adil@optym.com>
2023-03-08 13:41:25 +0530

    feat: bootstrap 11pixel app

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,8 @@
 import { Typography } from '@mui/material';
 import React from 'react';
 
-export const Home = () => {
+const Home = () => {
   return <Typography>Coming Soon</Typography>;
 };
+
+export default Home;
